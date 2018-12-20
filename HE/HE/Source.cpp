fefaab58f68b5286516b76e04f5cc91cbaacb14b
elemmire <351059171@qq.com>
2018-12-20 14:46:37 +0800

    histogram equalization for Y chanel

--- HE/HE/Source.cpp
+++ HE/HE/Source.cpp
@@ -6,5 +6,17 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
-	;
+	Mat src,dst,yuv;
+	vector<Mat> chanel(3);
+
+	src = imread("lena.jpg", IMREAD_COLOR);
+	imshow("before", src);
+	cvtColor(src,yuv,COLOR_BGR2YUV);
+
+	split(yuv, chanel);
+	equalizeHist(chanel[0], chanel[0]);
+	merge(chanel, yuv);
+	cvtColor(yuv, dst, COLOR_YUV2BGR);
+	imshow("after", dst);
+	waitKey(-1);
 }
