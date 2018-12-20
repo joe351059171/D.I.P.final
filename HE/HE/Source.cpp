ad22090c6331a00514fd5d7e13764ef9eb47531e
elemmire <351059171@qq.com>
2018-12-20 18:30:03 +0800

    add comments

--- HE/HE/Source.cpp
+++ HE/HE/Source.cpp
@@ -6,59 +6,57 @@ using namespace std;
 using namespace cv;
 
 Mat DrawHistogram(Mat src) {
-	MatND histo;
+	Mat histo;
 	int dims = 1;
-	float hranges[] = { 0, 255 };
-	const float *ranges[] = { hranges };
+	float range[] = { 0, 255 };
+	const float *ranges[] = { range };
 	int size = 256;
 	int channels[1] = { 0 };
+	//calcHist todo
 	calcHist(&src, 1, channels, Mat(),histo, dims, &size, ranges,true,false);
-	int scale = 1;
-	cv::Mat histPic(256 * scale, 256, CV_8U, cv::Scalar(255));
+	int scale = 2;
+	Mat dst(256 + 10, 256 * scale, CV_8U, Scalar(255));
 	double maxValue = 0;
 	double minValue = 0;
-	cv::minMaxLoc(histo, &minValue, &maxValue, NULL, NULL);
-	double rate = (256 / maxValue)*0.9;
-
+	minMaxLoc(histo, &minValue, &maxValue);
+	double rate = (256 / maxValue)*0.9;//value*rate is de facto (value/maxValue)*256[length of axis Y],0.9 to prevent reaching maximum
 	for (int i = 0; i < 256; i++)
 	{
 		float value = histo.at<float>(i);
-		cv::line(histPic, cv::Point(i*scale, 256), cv::Point(i*scale,256 - value * rate), cv::Scalar(0));
+		line(dst, Point(i*scale, 256), Point(i*scale,256 - value * rate), Scalar(0));
 	}
-	return histPic;
+	return dst;
 }
 
-/*
-Mat HistogramEqualize(Mat src,char* str) {
-	src = imread(str, 1);
-	vector<Mat>chanel(3);
+
+Mat HistogramEqualize(Mat src) {
+	vector<Mat>channel(3);
 	Mat yuv,dst;
+	Mat hist;
+	//cvtColor todo
 	cvtColor(src, yuv, COLOR_BGR2YUV);
-	split(yuv, chanel);
-	equalizeHist(chanel[0], chanel[0]);
-	merge(chanel, yuv);
+	//splt todo
+	split(yuv, channel);
+	hist = DrawHistogram(channel[0]);
+	imshow("histogram of origin", hist);
+	//equalizeHist todo
+	equalizeHist(channel[0], channel[0]);
+	hist = DrawHistogram(channel[0]);
+	imshow("histogram of HE", hist);
+	//merge todo
+	merge(channel, yuv);
 	cvtColor(yuv, dst, COLOR_YUV2BGR);
 	return dst;
 }
-*/
+
 
 int main(int argc, char** argv) {
-	Mat src,yuv,dst;
-	vector<Mat> channel(3);
-
-	src = imread("lena.jpg", IMREAD_COLOR);
+	Mat src,dst;
+	
+	src = imread(argv[1], IMREAD_COLOR);
 	imshow("before", src);
-	cvtColor(src,yuv,COLOR_BGR2YUV);
-	//split todo
-	split(yuv, channel);
-	Mat result = DrawHistogram(channel[0]);
-	imshow("txt", result);
-	//equalizehist todo
-	equalizeHist(channel[0], channel[0]);
-	result = DrawHistogram(channel[0]);
-	imshow("twt", result);
-	merge(channel, yuv);
-	cvtColor(yuv, dst, COLOR_YUV2BGR);
+	dst = HistogramEqualize(src);
 	imshow("after", dst);
+ 
 	waitKey(-1);
 }
